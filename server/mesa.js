87d782751fbd98c9818fe33ba02a537104832890
tmilar <tomasmilar2@hotmail.com>
2014-12-26 22:11:40 -0300

    Implementacion inicial y trivial de una mesa

--- server/mesa.js
+++ server/mesa.js
@@ -1,4 +1,26 @@
-var mesa = {
+var mesa = (function(){
+	jugadores: [],
 	cartasEnMesa: [],
-}
+
+	getJugador1:function  () {
+		return jugadores[0];
+	}
+	getJugador2: function () {
+		return jugadores[1];
+	}
+
+	agregarJugador: function () {
+		if(jugadores.length > 1){
+			errorHandler.error("La mesa esta llena, no se puede agregar mas jugadores!");
+			return;
+		}
+		jugadores.push()
+	}
+
+
+	return {
+		getJugador1: getJugador1,
+		getJugador2: getJugador2,
+	}
+})();
 
