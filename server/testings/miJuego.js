c576d7840641fea3b31c1a30bd45c03af91c8d4d
tmilar <tomasmilar2@hotmail.com>
2014-12-29 17:29:44 -0300

    Reorganizando estructura del proyecto, separacion 'cliente' y 'servidor'

---
+++ server/testings/miJuego.js
@@ -0,0 +1,14 @@
+
+//var miLogger = module['logger'];
+
+var juego = {
+	
+	cartas: 10,
+
+	usuario: "pepe",
+
+	iniciar: function(jugador1){
+		logger.log("empezo todo! el jugador "+ jugador1);
+	}
+
+}
