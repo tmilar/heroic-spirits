cecab4da9a72895c920d430fc646dc2a571510a5
tmilar <tomasmilar2@hotmail.com>
2014-12-29 21:44:26 -0300

    Aplicado Module Pattern al Jugador para esconder el estado privado. Tambien se agrego un metodo datosPublicosJugador() que devuelve los datos relevantes para mi oponente.

--- server/jugador.js
+++ server/jugador.js
@@ -2,16 +2,115 @@ var Session = require('./session');
 var Mazo = require('./mazo');
 var messageFactory = require('./messageFactory');
 
-var Jugador = function() {
-	return {
-		vidas: 30,
-		mano: [],
-		mazo: [], //obtener de BD
-		session: null,
+var Jugador = function Jugador() {
 
+	var vidas = 30;
+	var mano = [];
+	var mazo = null; //obtener de BD
+	var session = null;
+	var oponente = null;
+
+	var cartasMesa = null; //objeto Mesa..
+
+	var getId = function(){
+		return session.getId();
+	}
+
+	var datosPublicosJugador = function () {
+		return {
+			username: getUsername(),
+			vidas: vidas,
+			cantidadCartasMazo: mazo.cantidadCartasRestantes(),
+			cantidadCartasMano: mano.length
+		}
+	}
+
+	
+	/* //Private var datosPrivadosJugador= function () {
+			return {
+				nombreMazo: mazo.nombre,
+				mano: mano			
+			}
+		}
+	*/
+	var iniciarSesion = function(loginData, client){
+
+		//..... TODO : pedirle a la BD y validar LoginData ........
+		var loginValido = true;
+
+		if(!loginValido){
+			return null;
+		}
+
+		session = new Session(loginData, client);
+		mazo = Mazo.generar(25);//new Mazo(); //obtener de BD.
+
+		return session; 
+	}
+
+	var send = function(msj){
+		session.send(msj);
+	}
+
+	var robarCartas = function(cantidadCartas){
+
+		var cartas = mazo.robar(cantidadCartas);
+
+		for (var i = 0; i < cartas.length; i++) {
+			mano.push(cartas[i]);
+		};
+
+		send(messageFactory.msjRobarCartas(cartas, mazo.cantidadCartasRestantes()));
+	}
+
+
+	var bajarCarta = function (carta) {
+		cartasMano.splice(cartasMano.indexOf(carta), 1);
+		cartasJuego.push(carta);
+	}
+
+	var estaConectado = function () {
+		return !session.isClosed(); // session != null;
+	}
+
+	var desconectar = function(){
+		session.cerrar();
+		//session = null;
+	}
+
+	var getUsername = function(){
+		return session.getUsername();
+	}
+
+	return {
+		getId: getId,
+		datosPublicosJugador: datosPublicosJugador,
+		iniciarSesion: iniciarSesion,
+		send: send,
+		robarCartas: robarCartas,
+		bajarCarta: bajarCarta,
+		estaConectado: estaConectado,
+		desconectar: desconectar,
+		getUsername: getUsername
+
+	}
+
+	/*
+	return {
 		getId: function(){
 			return this.session.getId();
 		},
+
+		datosJugador: function () {
+			return {
+				username: session.getUsername(),
+				vidas: 
+				nombreMazo: mazo.nombre;
+				cartasMazo: mazo.cantidadCartas();
+				cartasMano: mano.length;
+
+			}
+		}
 
 		iniciarSesion: function(loginData, client){
 
@@ -61,7 +160,7 @@ var Jugador = function() {
 		getUsername: function(){
 			return this.session.getUsername();
 		}
-	}
+	} */
 }
 
 module.exports = Jugador;
