1691ef62016c2da2991c5d4d4316ca34b9fd15f7
tmilar <tomasmilar2@hotmail.com>
2014-12-29 21:42:27 -0300

    Agregado tipo "BusinessError" generico a aquellos errores que no tengan un nombre especifico

--- server/errorHandler.js
+++ server/errorHandler.js
@@ -5,8 +5,10 @@ var errorHandler = (function(){
 	errorStack = [];
 	return {
 		error: function(msjError){
-			errorStack.push(msjError);
-			logger.logError(msjError);
+
+			var errObj = {name: "BusinessError", message: msjError};
+			errorStack.push(errObj);
+			logger.logError(errObj);
 		},
 		imprimirErrorStack: function(){
 			for (var i = errorStack.length - 1; i >= 0; i--) {
