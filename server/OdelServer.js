9862866fc624bbd912ff4b05ed02d884cbfe34e1
tmilar <tomasmilar2@hotmail.com>
2014-12-29 21:41:53 -0300

    Refactorizado Server para mayor claridad y cohesividad.

--- server/OdelServer.js
+++ server/OdelServer.js
@@ -15,23 +15,18 @@ var loginModule = require('./loginModule');
 var juego = require('./juego');
 
 
-
-
-
-
-
-serverWs.onconnection = function(client) {
-
-    if (juego.mesaLlena()) {
-
-        logger.log("Se conecto un cliente pero la mesa ya esta llena. Conexion rechazada.");
-        client.send(messageFactory.msjJuegoComenzado());
-        client.close();
-    } 
-    else {
-        logger.log("Se conecto un cliente. Esperando identificacion. ");
-        //juego.registrarConexion(client);
+serverWs.validarNuevaConexion = function() {
+    if(juego.puedenEntrarJugadores()){
+        throw {
+            name: "JuegoComenzadoError",
+            message: "Se conecto un cliente pero la mesa ya esta llena. Conexion rechazada."
+        };
     }
+}
+
+serverWs.aceptarCliente = function(client){
+
+    logger.log("Se conecto un cliente exitosamente. Esperando identificacion. ");
 
     client.onmessage = function(event) {
 
@@ -40,33 +35,59 @@ serverWs.onconnection = function(client) {
         //logger.log("CLIENTE WSS : " + JSON.stringify(mysocket._socket.address() ));
         //logger.log("CLIENTE WSS : " +mysocket._socket.remoteAddress  );
         //logger.log("CLIENTE WSS : " +mysocket._socket.remotePort  );
-
-
         logger.log("Mensaje recibido: " + event.data +", del cliente: "+JSON.stringify(mysocket.address()));
         
         messageHandler.procesar(JSON.parse(event.data), this);
     };
 
+
     client.onerror = function(event) {
-        var sessionCliente;
-        if( sessionCliente = this.getWss().session) {
+
+        var sessionCliente = this.desconectarJugador();
+
+        if( sessionCliente) 
             logger.log("Se desconecto un cliente por un error, sesion: " + sessionCliente.toString());
-            juego.desconectarJugador(sessionCliente);
-        }
         else
             logger.log("Se desconecto por error, un cliente no identificado.");
 
     };
 
     client.onclose = function(event) {
-        var sessionCliente;
-        if( sessionCliente = this.getWss().session) {
-            logger.log("Un cliente cerro la conexion, sesion: " + sessionCliente.toString());
-            juego.desconectarJugador(sessionCliente);
-        }
+        var sessionCliente = this.desconectarJugador();
+
+        if( sessionCliente ) 
+            logger.log("Un cliente cerro la conexion, sesion: " + sessionCliente.toString()); 
         else
             logger.log("Un cliente no identificado cerro la conexion.");
     };
+
+
+    client.desconectarJugador = function(){
+        var session;
+        if(session = this.getWss().session)
+            juego.desconectarJugador(session);
+        
+        return session;
+    };
+}
+
+
+serverWs.onconnection = function(client) {
+
+
+    try{
+        this.validarNuevaConexion();
+        this.aceptarCliente(client);
+
+    }catch(e){
+
+        logger.logError(e);
+
+        if(e.name === "JuegoComenzadoError") 
+            client.send(messageFactory.msjJuegoComenzado()); 
+
+        client.close();        
+    }
 };
 
 logger.log("Iniciado servidor. Esperando conexiones...");
